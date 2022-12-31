d511198191d373a25f1c153f0ee0bd5d7533653d
Adam Johnson <me@adamj.eu>
2022-12-31 09:46:35 +0000

    Subclassing

--- example/tests.py
+++ example/tests.py
@@ -2,23 +2,29 @@ import django
 from django import test
 
 if django.VERSION < (4, 2):
-    # Backport https://code.djangoproject.com/ticket/34063
-
+    # Backport of https://code.djangoproject.com/ticket/34063
     from django.core.handlers.wsgi import LimitedStream
-    from django.test.client import AsyncClientHandler
+    from django.test.client import AsyncClient, AsyncClientHandler
 
-    orig_get_response_async = AsyncClientHandler.get_response_async
+    class FixedAsyncClientHandler(AsyncClientHandler):
+        async def get_response_async(self, request):
+            request._stream = LimitedStream(
+                request._stream,
+                len(request._stream),
+            )
+            return await super().get_response_async(request)
 
-    async def get_response_async(self, request):
-        request._stream = LimitedStream(
-            request._stream,
-            len(request._stream),
-        )
-        return await orig_get_response_async(self, request)
-
-    AsyncClientHandler.get_response_async = get_response_async
+    class FixedAsyncClient(AsyncClient):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.handler.__class__ = FixedAsyncClientHandler
 
 
-class IndexTests(test.TestCase):
+class TestCase(test.TestCase):
+    if django.VERSION < (4, 2):
+        async_client_class = FixedAsyncClient
+
+
+class IndexTests(TestCase):
     async def test_post(self):
         await self.async_client.post("/")
